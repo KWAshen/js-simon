0352648b6d8b7716c381c38cc4d39d613e2d7524
KWAshen <ashen.maliaha@gmail.com>
2025-07-17 17:49:35 +0200

    lista dei elementi

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const numberslist = document.getElementById('numbers-list');
+const form = document.getElementById('answers-form');
+const inputs = document.querySelectorAll('input');
+const countdown = document.getElementById('countdown');
+const message = document.getElementById('message');
+const instructions = document.getElementById('instructions');
+const button = document.getElementById('button');
+
